31373b307cd95ef920d4a85c3bbfbd87dcd918c5
James Summers <jamesnsummers@gmail.com>
2017-07-07 10:56:42 -0500

    testing conditionals in index.html

--- Marathonr/main_app/views.py
+++ Marathonr/main_app/views.py
@@ -6,13 +6,14 @@ def index(request):
     return render(request, 'index.html', {'movies': movies})
 
 class Movie:
-    def __init__(self, title, theatre):
+    def __init__(self, title, theatre, showtimes):
         self.title = title
         self.theatre = theatre
+        self.showtimes = showtimes
 
 
 movies = [
-    Movie('Baby Driver', 'Alamo Drafthouse Mueller'),
-    Movie('The Big Sick', 'Violet Crown Cinema'),
-    Movie('Wonder Woman', 'Alamo Drafthouse South Lamar')
+    Movie('Baby Driver', 'Alamo Drafthouse Mueller', '12:45pm, 3:30pm, 6:10pm, 7:30pm, 9:30pm, 10:35pm'),
+    Movie('The Big Sick', 'Violet Crown Cinema', '11:00am, 1:45pm, 4:00pm, 6:30pm, 9:20pm, 10:45pm'),
+    Movie('Wonder Woman', 'Alamo Drafthouse South Lamar', '' )
 ]
