d00afc3ccd09b06076806f997505cee314b06fb5
James Summers <jamesnsummers@gmail.com>
2017-07-07 10:48:57 -0500

    added some seed data to views.py to test

--- Marathonr/main_app/views.py
+++ Marathonr/main_app/views.py
@@ -1,7 +1,18 @@
 """views file for main_app"""
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    """index function to show or GET all"""
-    return HttpResponse('<h1>Hello Cinefiles!</h1>')
+    """function to show index"""
+    return render(request, 'index.html', {'movies': movies})
+
+class Movie:
+    def __init__(self, title, theatre):
+        self.title = title
+        self.theatre = theatre
+
+
+movies = [
+    Movie('Baby Driver', 'Alamo Drafthouse Mueller'),
+    Movie('The Big Sick', 'Violet Crown Cinema'),
+    Movie('Wonder Woman', 'Alamo Drafthouse South Lamar')
+]
